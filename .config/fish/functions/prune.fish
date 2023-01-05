f3548b526bdbbf0a61f7ef60f808f6aa92705f3a
Tom Fleet <tomfleet2018@gmail.com>
2023-01-05 11:58:11 +0000

    Sync: 2023-01-05 11:58:11

--- .config/fish/functions/prune.fish
+++ .config/fish/functions/prune.fish
@@ -1,21 +1,30 @@
 #!/usr/bin/env fish
 
 function prune --description "Prunes all local branches that have been merged."
-    argparse --min-args 0 --max-args 1 h/help -- $argv
+    argparse --min-args 0 --max-args 1 h/help f/force -- $argv
     or return 1
 
     if set -q _flag_help
-        echo "Prunes all local branches that have been merged"
+        echo "Prunes local git branches."
         echo
         echo (set_color --bold)"Usage:"(set_color normal)
         echo "	prune"
         echo
         echo (set_color --bold)"Flags:"(set_color normal)
-        echo "	[-h/--help]: Show this help message and exit."
+        echo "  [-h/--help]: Show this help message and exit."
+        echo "  [-f/--force]: Remove ALL local branches."
         echo
         return 0
     end
-    # Function body goes here
-    git branch --merged | grep -v '\*' | xargs git branch -d
+
+    if set -q _flag_force
+        echo "Removing ALL local branches"
+        git branch | grep -v '\*' | xargs git branch -D
+        return 0
+    end
+
+    echo "Removing all local branches that have been merged"
+    git branch | grep -v '\*' | xargs git branch -D
+    return 0
 
 end
