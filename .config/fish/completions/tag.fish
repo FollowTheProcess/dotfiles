08ded5e5612618deab492c5b2dea012fa3ba75b1
Tom Fleet <tomfleet2018@gmail.com>
2023-01-14 18:30:27 +0000

    Sync: 2023-01-14 18:30:27

--- .config/fish/completions/tag.fish
+++ .config/fish/completions/tag.fish
@@ -1,29 +1,27 @@
-#!/usr/bin/env fish
+# fish completion for tag                                  -*- shell-script -*-
 
-
-# fish completion for gotoil                               -*- shell-script -*-
-
-function __gotoil_debug
+function __tag_debug
     set -l file "$BASH_COMP_DEBUG_FILE"
     if test -n "$file"
-        echo "$argv" >>$file
+        echo "$argv" >> $file
     end
 end
 
-function __gotoil_perform_completion
-    __gotoil_debug "Starting __gotoil_perform_completion"
+function __tag_perform_completion
+    __tag_debug "Starting __tag_perform_completion"
 
     # Extract all args except the last one
     set -l args (commandline -opc)
     # Extract the last arg and escape it in case it is a space
     set -l lastArg (string escape -- (commandline -ct))
 
-    __gotoil_debug "args: $args"
-    __gotoil_debug "last arg: $lastArg"
+    __tag_debug "args: $args"
+    __tag_debug "last arg: $lastArg"
 
-    set -l requestComp "$args[1] __complete $args[2..-1] $lastArg"
+    # Disable ActiveHelp which is not supported for fish shell
+    set -l requestComp "TAG_ACTIVE_HELP=0 $args[1] __complete $args[2..-1] $lastArg"
 
-    __gotoil_debug "Calling $requestComp"
+    __tag_debug "Calling $requestComp"
     set -l results (eval $requestComp 2> /dev/null)
 
     # Some programs may output extra empty lines after the directive.
@@ -46,9 +44,9 @@ function __gotoil_perform_completion
     # completions must be prefixed with the flag
     set -l flagPrefix (string match -r -- '-.*=' "$lastArg")
 
-    __gotoil_debug "Comps: $comps"
-    __gotoil_debug "DirectiveLine: $directiveLine"
-    __gotoil_debug "flagPrefix: $flagPrefix"
+    __tag_debug "Comps: $comps"
+    __tag_debug "DirectiveLine: $directiveLine"
+    __tag_debug "flagPrefix: $flagPrefix"
 
     for comp in $comps
         printf "%s%s\n" "$flagPrefix" "$comp"
@@ -58,29 +56,29 @@ function __gotoil_perform_completion
 end
 
 # This function does two things:
-# - Obtain the completions and store them in the global __gotoil_comp_results
+# - Obtain the completions and store them in the global __tag_comp_results
 # - Return false if file completion should be performed
-function __gotoil_prepare_completions
-    __gotoil_debug ""
-    __gotoil_debug "========= starting completion logic =========="
+function __tag_prepare_completions
+    __tag_debug ""
+    __tag_debug "========= starting completion logic =========="
 
     # Start fresh
-    set --erase __gotoil_comp_results
+    set --erase __tag_comp_results
 
-    set -l results (__gotoil_perform_completion)
-    __gotoil_debug "Completion results: $results"
+    set -l results (__tag_perform_completion)
+    __tag_debug "Completion results: $results"
 
     if test -z "$results"
-        __gotoil_debug "No completion, probably due to a failure"
+        __tag_debug "No completion, probably due to a failure"
         # Might as well do file completion, in case it helps
         return 1
     end
 
     set -l directive (string sub --start 2 $results[-1])
-    set --global __gotoil_comp_results $results[1..-2]
+    set --global __tag_comp_results $results[1..-2]
 
-    __gotoil_debug "Completions are: $__gotoil_comp_results"
-    __gotoil_debug "Directive is: $directive"
+    __tag_debug "Completions are: $__tag_comp_results"
+    __tag_debug "Directive is: $directive"
 
     set -l shellCompDirectiveError 1
     set -l shellCompDirectiveNoSpace 2
@@ -94,7 +92,7 @@ function __gotoil_prepare_completions
 
     set -l compErr (math (math --scale 0 $directive / $shellCompDirectiveError) % 2)
     if test $compErr -eq 1
-        __gotoil_debug "Received error directive: aborting."
+        __tag_debug "Received error directive: aborting."
         # Might as well do file completion, in case it helps
         return 1
     end
@@ -102,7 +100,7 @@ function __gotoil_prepare_completions
     set -l filefilter (math (math --scale 0 $directive / $shellCompDirectiveFilterFileExt) % 2)
     set -l dirfilter (math (math --scale 0 $directive / $shellCompDirectiveFilterDirs) % 2)
     if test $filefilter -eq 1; or test $dirfilter -eq 1
-        __gotoil_debug "File extension filtering or directory filtering not supported"
+        __tag_debug "File extension filtering or directory filtering not supported"
         # Do full file completion instead
         return 1
     end
@@ -110,7 +108,7 @@ function __gotoil_prepare_completions
     set -l nospace (math (math --scale 0 $directive / $shellCompDirectiveNoSpace) % 2)
     set -l nofiles (math (math --scale 0 $directive / $shellCompDirectiveNoFileComp) % 2)
 
-    __gotoil_debug "nospace: $nospace, nofiles: $nofiles"
+    __tag_debug "nospace: $nospace, nofiles: $nofiles"
 
     # If we want to prevent a space, or if file completion is NOT disabled,
     # we need to count the number of valid completions.
@@ -119,22 +117,22 @@ function __gotoil_prepare_completions
     # criteria than the prefix.
     if test $nospace -ne 0; or test $nofiles -eq 0
         set -l prefix (commandline -t | string escape --style=regex)
-        __gotoil_debug "prefix: $prefix"
+        __tag_debug "prefix: $prefix"
 
-        set -l completions (string match -r -- "^$prefix.*" $__gotoil_comp_results)
-        set --global __gotoil_comp_results $completions
-        __gotoil_debug "Filtered completions are: $__gotoil_comp_results"
+        set -l completions (string match -r -- "^$prefix.*" $__tag_comp_results)
+        set --global __tag_comp_results $completions
+        __tag_debug "Filtered completions are: $__tag_comp_results"
 
         # Important not to quote the variable for count to work
-        set -l numComps (count $__gotoil_comp_results)
-        __gotoil_debug "numComps: $numComps"
+        set -l numComps (count $__tag_comp_results)
+        __tag_debug "numComps: $numComps"
 
         if test $numComps -eq 1; and test $nospace -ne 0
             # We must first split on \t to get rid of the descriptions to be
             # able to check what the actual completion will be.
             # We don't need descriptions anyway since there is only a single
             # real completion which the shell will expand immediately.
-            set -l split (string split --max 1 \t $__gotoil_comp_results[1])
+            set -l split (string split --max 1 \t $__tag_comp_results[1])
 
             # Fish won't add a space if the completion ends with any
             # of the following characters: @=/:.,
@@ -142,16 +140,16 @@ function __gotoil_prepare_completions
             if not string match -r -q "[@=/:.,]" -- "$lastChar"
                 # In other cases, to support the "nospace" directive we trick the shell
                 # by outputting an extra, longer completion.
-                __gotoil_debug "Adding second completion to perform nospace directive"
-                set --global __gotoil_comp_results $split[1] $split[1].
-                __gotoil_debug "Completions are now: $__gotoil_comp_results"
+                __tag_debug "Adding second completion to perform nospace directive"
+                set --global __tag_comp_results $split[1] $split[1].
+                __tag_debug "Completions are now: $__tag_comp_results"
             end
         end
 
         if test $numComps -eq 0; and test $nofiles -eq 0
             # To be consistent with bash and zsh, we only trigger file
             # completion when there are no other completions
-            __gotoil_debug "Requesting file completion"
+            __tag_debug "Requesting file completion"
             return 1
         end
     end
@@ -163,16 +161,17 @@ end
 # so we can properly delete any completions provided by another script.
 # Only do this if the program can be found, or else fish may print some errors; besides,
 # the existing completions will only be loaded if the program can be found.
-if type -q gotoil
+if type -q "tag"
     # The space after the program name is essential to trigger completion for the program
     # and not completion of the program name itself.
     # Also, we use '> /dev/null 2>&1' since '&>' is not supported in older versions of fish.
-    complete --do-complete "gotoil " >/dev/null 2>&1
+    complete --do-complete "tag " > /dev/null 2>&1
 end
 
 # Remove any pre-existing completions for the program since we will be handling all of them.
-complete -c gotoil -e
+complete -c tag -e
 
-# The call to __gotoil_prepare_completions will setup __gotoil_comp_results
+# The call to __tag_prepare_completions will setup __tag_comp_results
 # which provides the program's completion choices.
-complete -c gotoil -n __gotoil_prepare_completions -f -a '$__gotoil_comp_results'
+complete -c tag -n '__tag_prepare_completions' -f -a '$__tag_comp_results'
+
