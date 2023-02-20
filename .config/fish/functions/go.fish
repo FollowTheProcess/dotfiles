2d6fab27e6f9a1324c71860c20d613a04cc6a4ca
Tom Fleet <tomfleet2018@gmail.com>
2023-02-20 20:35:37 +0000

    Sync: 2023-02-20 20:35:37

---
+++ .config/fish/functions/go.fish
@@ -0,0 +1,26 @@
+#!/usr/bin/env fish
+
+function go --wraps="go" --description "Wrapper around the Go command to add a new subcommand"
+    if test (count $argv) -eq 2
+        and test $argv[1] = new
+        set_color cyan
+        echo "Creating new project $argv[2]"
+        set_color normal
+
+        set -l name $argv[2]
+
+        mkdir $name
+        cd $name
+        echo >main.go "\
+package main
+
+import \"fmt\"
+
+func main() {
+    fmt.Println(\"Hello, World!\")
+}"
+        command go mod init github.com/FollowTheProcess/$name
+    else
+        command go $argv
+    end
+end
