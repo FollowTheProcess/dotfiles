93d5f005d1f6ea117008bc4ada88cf304ce91cea
Tom Fleet <tomfleet2018@gmail.com>
2023-04-01 15:02:12 +0100

    Sync: 2023-04-01 15:02:12

--- .config/fish/functions/go.fish
+++ .config/fish/functions/go.fish
@@ -2,27 +2,22 @@
 
 function go --wraps="go" --description "Wrapper around the Go command to add a new subcommand"
     if test (count $argv) -eq 2
-        and test $argv[1] = new
-        set_color cyan
-        echo "Creating new project $argv[2]"
-        set_color normal
+        if test $argv[1] = new
+            set_color cyan
+            echo "Creating new project $argv[2]"
+            set_color normal
 
-        set -l name $argv[2]
+            set -l name $argv[2]
 
-        mkdir $name
-        cd $name
-        echo >main.go "\
-package main
+            mkdir $name
+            cd $name
+            cp ~/.config/fish/functions/assets/main.go .
+            command go mod init github.com/FollowTheProcess/$name
+            command git init
+            command git add .
+            command git commit -m "Initial commit"
 
-import \"fmt\"
-
-func main() {
-    fmt.Println(\"Hello, World!\")
-}"
-        command go mod init github.com/FollowTheProcess/$name
-        command git init
-        command git add .
-        command git commit -m "Initial commit"
+        end
     else
         command go $argv
     end
