fcf3daf7701b5d027c6cceb60b3b691bc1ac9537
Tom Fleet <tomfleet2018@gmail.com>
2023-06-05 08:01:28 +0100

    Sync: 2023-06-05 08:01:28

--- .config/fish/functions/copier.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-function copier --wraps="copier" --description "Wraps copier with a few defaults"
-
-    command copier --data="github_username=FollowTheProcess" --data="author_name=Tom Fleet" --data="author_email=tomfleet2018@gmail.com" $argv
-
-end
