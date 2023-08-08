8189627db837ca7feb1609b0f9457746d042d3a5
Tom Fleet <tomfleet2018@gmail.com>
2023-08-08 19:37:45 +0100

    Sync: 2023-08-08 19:37:45

--- .config/fish/functions/pod.fish
+++ .config/fish/functions/pod.fish
@@ -1,5 +1,24 @@
 #!/usr/bin/env fish
 
-function pod --description "Create a new POD ticket branch."
-    python ~/.config/fish/functions/helpers/pod.py $argv
+function pod --description "Create a new POD ticket."
+    argparse --min-args 0 --max-args 0 h/help -- $argv
+    or return 1
+
+    if set -q _flag_help
+        echo "Create a new POD ticket"
+        echo
+        echo (set_color --bold)"Usage:"(set_color normal)
+        echo "	pod"
+        echo
+        echo (set_color --bold)"Flags:"(set_color normal)
+        echo "	[-h/--help]: Show this help message and exit."
+        echo
+        return 0
+    end
+    # Function body goes here
+    set TYPE (gum choose --header "Type of branch?" "feature" "fix" "ci" "chore" "docs" "refactor" "deps" "release")
+    set TICKET (gum input --placeholder "Jira ticket number")
+
+    command git switch --create $TYPE/$TICKET
+
 end
