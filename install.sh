6823c68929a494dc79b60a8e9add76754d42f4c8
Tom Fleet <tomfleet2018@gmail.com>
2024-07-06 09:37:59 +0100

    Sync: 2024-07-06 09:37:59

--- install.sh
+++ install.sh
@@ -56,6 +56,7 @@ wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/
 wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme
 wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Macchiato.tmTheme
 wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
+bat cache --build
 
 # macOS Preferences
 echo -e "${GREEN}Configuring macOS preferences...${NC}"
