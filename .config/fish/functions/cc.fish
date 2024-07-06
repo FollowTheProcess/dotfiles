7f260286ba9186122b2e3fe9f1bd16a12a1c1a6b
Tom Fleet <tomfleet2018@gmail.com>
2024-07-06 16:18:10 +0100

    Sync: 2024-07-06 16:18:10

--- .config/fish/functions/cc.fish
+++ .config/fish/functions/cc.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
-function cc --wraps=cookiecutter --description 'alias cc=cookiecutter'
-    command cookiecutter $argv
+function cc --wraps='copier copy' --description 'alias cc=copier copy'
+    command copier copy --data "github_username=FollowTheProcess" --data "author_name=Tom Fleet" --data "author_email=tomfleet2018@gmail.com" $argv
 end
