60ca0a92dd024e05b319eb909228ed51e4b6c13d
Tom Fleet <tomfleet2018@gmail.com>
2024-08-21 17:38:16 +0100

    Sync: 2024-08-21 17:38:16

--- pipx_install.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-pipx ensurepath
-
-for package in build codespell copier datamodel-code-generator httpie mypy nox pdm poetry pre-commit ruff tbump uv virtualenv
-    pipx install $package
-end
