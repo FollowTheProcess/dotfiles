86edd4f65e84754e7d5944c24447fe13e8de4203
Tom Fleet <tomfleet2018@gmail.com>
2024-08-22 18:42:05 +0100

    Sync: 2024-08-22 18:42:05

--- install.sh
+++ install.sh
@@ -37,11 +37,8 @@ touch ~/.hushprompt
 
 # Copy all the things!
 cp -r .config $HOME/
-cp .cookiecutterrc $HOME/
-cp .condarc $HOME/
 cp .gitconfig $HOME/
 cp .gitignore $HOME/
-cp .pypirc $HOME/
 cp .pytoil.toml $HOME/
 cp -r hatch $HOME/Library/Application\ Support/hatch
 cp -r pypoetry $HOME/Library/Application\ Support/pypoetry
@@ -53,14 +50,12 @@ mkdir -p ~/Development
 
 # Themes
 mkdir -p "$(bat --config-dir)/themes"
-wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Latte.tmTheme
 wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme
 wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Macchiato.tmTheme
 wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
 bat cache --build
 
 mkdir -p ~/.config/alacritty
-curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-latte.toml
 curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-frappe.toml
 curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-macchiato.toml
 curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
