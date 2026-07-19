088f4743d1f21b89e6d9b34ee51aab809a6fc6c8
Tom Fleet <me@followtheprocess.codes>
2026-07-19 16:05:35 +0100

    Fix unquoted space in ssh.nix

--- nix/modules/home/programs/ssh.nix
+++ nix/modules/home/programs/ssh.nix
@@ -5,7 +5,8 @@
 
     settings = {
       "*" = {
-        IdentityAgent = "${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+        # Quoted because 'Group Containers' contains a space
+        IdentityAgent = ''"${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
       };
       "tangled.org" = {
         HostName = "tangled.org";
