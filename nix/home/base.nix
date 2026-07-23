283d134922712277e100b3a2d9cc149fb1464e3f
Tom Fleet <me@followtheprocess.codes>
2026-07-23 21:01:24 +0100

    Put back the home-manager enable line

--- nix/home/base.nix
+++ nix/home/base.nix
@@ -3,5 +3,8 @@
   flake.modules.homeManager.base = {
     imports = [ inputs.self.modules.generic.constants ];
     home.stateVersion = "26.11";
+
+    # Let home manager install and manage itself
+    programs.home-manager.enable = true;
   };
 }
