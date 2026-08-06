7b7e1d1bd10f7610e661c6abb4623e63545b39f7
Tom Fleet <me@followtheprocess.codes>
2026-08-06 20:33:16 +0100

    Follow nixpkgs-lib for flake-parts

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
     };
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
