3c21f72c496c64fd14e45e9e361129831f88ec08
Tom Fleet <me@followtheprocess.codes>
2026-08-07 13:41:55 +0100

    Implement Aikido cert fixes

--- nix/hosts/work.nix
+++ nix/hosts/work.nix
@@ -23,43 +23,10 @@ in
 
     # Endpoint protection intercepts TLS with a MITM CA to scan for
     # supply chain vulnerabilities. This setting covers nix fetches
-    #
-    # Build sandboxes are handled below
     nix.settings = {
-      pure-eval = false; # Needed as the path below is out of store
-      ssl-cert-file = "/etc/ssl/certs/aikido-nix-ca.pem";
+      experimental-features = [ "configurable-impure-env" ];
+      impure-env = [ "NIX_SSL_CERT_FILE=/etc/nix/aikido-bundle.crt" ];
     };
-
-    # catppuccin-nix's whiskers fetches cargo crates over HTTPS during the
-    # build, which Aikido intercepts.
-    #
-    # If I override cacert to trust it, it invalidates the cache of basically
-    # everything else in the nix ecosystem, requiring a full build of the
-    # universe from source.
-    #
-    # So I've overridden it just for catppuccin-nix which seems to work
-    home-manager.sharedModules = [
-      (
-        { pkgs, ... }:
-        let
-          trustedPkgs = pkgs.extend (
-            _final: prev: {
-              cacert = prev.cacert.override {
-                extraCertificateFiles = [
-                  (builtins.path {
-                    path = "/etc/ssl/certs/aikido-nix-ca.pem";
-                    name = "aikido-ca.pem";
-                  })
-                ];
-              };
-            }
-          );
-        in
-        {
-          catppuccin.sources = (import "${inputs.catppuccin}/default.nix" { pkgs = trustedPkgs; }).packages;
-        }
-      )
-    ];
 
     home-manager.users.tomfleet.imports = with inputs.self.modules.homeManager; [
       work-home
