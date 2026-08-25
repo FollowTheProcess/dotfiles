b5e055ea8d7d7f31518a3eef3bc9e86587297f5d
Tom Fleet <me@followtheprocess.codes>
2026-08-25 13:01:13 +0100

    Use Go 1.27

--- nix/home/programs/go.nix
+++ nix/home/programs/go.nix
@@ -1,16 +1,24 @@
 {
-  flake.modules.homeManager.base = { config, lib, ... }: {
-    programs.go = {
-      enable = true;
-      env = {
-        GOPATH = "${config.home.homeDirectory}/go";
-        GOBIN = "${config.home.homeDirectory}/go/bin";
-        GOEXPERIMENT = "jsonv2";
-        CGO_ENABLED = "0";
-      }
-      // lib.optionalAttrs (config.my.go.private != [ ]) {
-        GOPRIVATE = lib.concatStringsSep "," config.my.go.private;
+  flake.modules.homeManager.base =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      programs.go = {
+        enable = true;
+        package = pkgs.go_1_27;
+        env = {
+          GOPATH = "${config.home.homeDirectory}/go";
+          GOBIN = "${config.home.homeDirectory}/go/bin";
+          GOEXPERIMENT = "jsonv2";
+          CGO_ENABLED = "0";
+        }
+        // lib.optionalAttrs (config.my.go.private != [ ]) {
+          GOPRIVATE = lib.concatStringsSep "," config.my.go.private;
+        };
       };
     };
-  };
 }
